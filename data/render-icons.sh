fb52894e4e1055edcb3510251d5f9056c9cc4504
IBBoard <dev@ibboard.co.uk>
2019-08-03 21:03:27 +0100

    Rebrand the logo to look crow-like
    Caw-caw!

--- data/render-icons.sh
+++ data/render-icons.sh
@@ -7,5 +7,5 @@ sizes=(16  24 32 48 64 96 128 256)
 for size in ${sizes[@]}
 do
   rsvg-convert ./cawbird.svg --width="${size}" --height="${size}" \
-               --format=png -o "./hicolor/${size}x${size}/cawbird.png"
+               --format=png -o "./hicolor/${size}x${size}/apps/cawbird.png"
 done
