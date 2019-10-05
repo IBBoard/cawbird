89aa9c9731aa048f7ed3f2f0c04da20dae02845c
IBBoard <dev@ibboard.co.uk>
2019-10-05 18:45:31 +0100

    Replace bigger PNGs with SVG
    Suggested in #23. I hadn't realised we weren't shipping the SVG.
    128px and above should work okay with the SVG. Below that use PNG.

--- data/render-icons.sh
+++ data/render-icons.sh
@@ -2,10 +2,12 @@
 
 # Oh god I hate bash.
 
-sizes=(16  24 32 48 64 96 128 256)
+sizes=(16  24 32 48 64 96)
 
 for size in ${sizes[@]}
 do
   rsvg-convert ./uk.co.ibboard.cawbird.svg --width="${size}" --height="${size}" \
                --format=png -o "./hicolor/${size}x${size}/apps/uk.co.ibboard.cawbird.png"
 done
+
+cp ./uk.co.ibboard.cawbird.svg ./hicolor/scalable/apps/uk.co.ibboard.cawbird.svg
