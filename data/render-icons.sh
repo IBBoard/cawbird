85682b2c5b39e5fca5896728e3bc3af21a80f678
IBBoard <dev@ibboard.co.uk>
2019-10-05 18:40:41 +0100

    Re #24: Standardise icon names
    The convention is now Java package-like "reverse DNS" notation
    
    Standardise on capitalisation of existing files

--- data/render-icons.sh
+++ data/render-icons.sh
@@ -6,6 +6,6 @@ sizes=(16  24 32 48 64 96 128 256)
 
 for size in ${sizes[@]}
 do
-  rsvg-convert ./cawbird.svg --width="${size}" --height="${size}" \
-               --format=png -o "./hicolor/${size}x${size}/apps/cawbird.png"
+  rsvg-convert ./uk.co.ibboard.cawbird.svg --width="${size}" --height="${size}" \
+               --format=png -o "./hicolor/${size}x${size}/apps/uk.co.ibboard.cawbird.png"
 done
